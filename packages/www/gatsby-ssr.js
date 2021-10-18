c651c73107f4495e16adcc51a71c2cc3a37fa416
bugattie <m.mohsinbarkaati@gmail.com>
2021-10-18 14:12:58 +0500

    build issue

--- packages/www/gatsby-ssr.js
+++ packages/www/gatsby-ssr.js
@@ -1,4 +1,18 @@
-const React = require("react");
 const wrapRootElement = require("./wrap-root-element");
+const React = require("react");
 
-exports.wrapRootElement = wrapRootElement;
+const { ApolloProvider, ApolloClient, HttpLink } = require("@apollo/client");
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://bugattie-todo.netlify.app/.netlify/functions/todo",
+  }),
+});
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <ApolloProvider client={client}>
+      {wrapRootElement({ element })}
+    </ApolloProvider>
+  );
+};
